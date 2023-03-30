5f13877fb7ea9c163ffe9466bc6ca870f5f253ac
Lyndon Obbus <lyndon.obbus@gmail.com>
2023-03-31 07:21:00 +0800

    Add Navbar

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,9 +1,12 @@
 import { Grid, GridItem } from "@hope-ui/solid"
 import type { Component } from 'solid-js'
+import NavBar from "./components/Navbar"
 
 const App: Component = () => (
     <Grid templateAreas={`"nav nav" "aside main"`}>
-        <GridItem area="nav" bg="papayawhip">Nav</GridItem>
+        <GridItem area="nav">
+            <NavBar />
+        </GridItem>
         <GridItem area="aside" bg="tomato">Aside</GridItem>
         <GridItem area="main" bg="papayawhip">Main</GridItem>
     </Grid>
---
+++ apps/client/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import { HStack, Text } from "@hope-ui/solid"
+import type { Component } from 'solid-js'
+
+const NavBar: Component = () => (
+    <HStack>
+        <Text>Tickets Counter</Text>
+    </HStack>
+)
+
+export default NavBar
