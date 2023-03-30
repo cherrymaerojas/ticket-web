eb4d8120a2fb6923cc80851ff38e53bec52b2783
Lyndon Obbus <lyndon.obbus@gmail.com>
2023-03-31 03:17:47 +0800

    Add responsive layout

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,12 @@
+import { Grid, GridItem } from "@hope-ui/solid"
 import type { Component } from 'solid-js'
 
 const App: Component = () => (
-    <h1>Hello..</h1>
+    <Grid templateAreas={`"nav nav" "aside main"`}>
+        <GridItem area="nav" bg="papayawhip">Nav</GridItem>
+        <GridItem area="aside" bg="tomato">Aside</GridItem>
+        <GridItem area="main" bg="papayawhip">Main</GridItem>
+    </Grid>
 )
 
 export default App
