79819502a100aaacb83e67422fa86e812a0345f6
Lyndon Obbus <lyndon.obbus@gmail.com>
2023-03-31 03:04:06 +0800

    Replace React to SolidJS and added HopeUI

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,31 +1,7 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import type { Component } from 'solid-js'
 
-function App() {
-    const [greeting, setGreeting] = useState('')
-
-    useEffect(() => {
-        fetch('/api')
-            .then(res => res.text())
-            .then(setGreeting)
-    }, [])
-
-    return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <h1>{greeting}</h1>
-        </div>
-    )
-}
+const App: Component = () => (
+    <h1>Hello..</h1>
+)
 
 export default App
